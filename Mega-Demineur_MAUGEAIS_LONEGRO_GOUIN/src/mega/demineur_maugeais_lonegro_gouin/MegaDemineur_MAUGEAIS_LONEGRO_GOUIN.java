1cff6562828b769d184a187e4968cd84c6717556
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-12-07 12:08:15 +0100

    Création du projet

---
+++ Mega-Demineur_MAUGEAIS_LONEGRO_GOUIN/src/mega/demineur_maugeais_lonegro_gouin/MegaDemineur_MAUGEAIS_LONEGRO_GOUIN.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mega.demineur_maugeais_lonegro_gouin;
+
+/**
+ *
+ * @author thoma
+ */
+public class MegaDemineur_MAUGEAIS_LONEGRO_GOUIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
